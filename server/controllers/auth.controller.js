ce1ebebaa0a0ecf24cb14863dbd9670b7aa676a8
g3vind <g3vind@gmail.com>
2024-03-21 14:36:16 +0530

    error handling middleware

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,19 +1,19 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
 
   // hashing password in the database
   const hashedPassword = bcryptjs.hashSync(password, 10)
 
-  //   create a new user using our user model
+  // create a new user using our user model
   const newUser = new User({ username, email, password: hashedPassword })
 
   try {
     await newUser.save()
     res.status(201).json({ message: "User created successfully" })
   } catch (error) {
-    res.status(500).json({ message: "Error in creating a new user" })
+    next(error)
   }
 }
