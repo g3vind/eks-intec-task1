4016d62f04bd20e62632520d7c359615742882e2
g3vind <g3vind@gmail.com>
2024-03-21 12:57:09 +0530

    test api and hash password

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config()
+import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js"
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -13,8 +15,16 @@ mongoose
   })
 
 const app = express()
+app.use(express.json())
 const PORT = 5000
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`)
 })
+
+app.get("/", (req, res) => {
+  res.json({ message: "Success" })
+})
+
+app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes)
