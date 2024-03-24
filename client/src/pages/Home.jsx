72410412e714edbb5dfa6f88b75fbec5c25cef0c
g3vind <g3vind@gmail.com>
2024-03-25 00:02:01 +0530

    deployment changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 
 const Home = () => {
   const { currentUser } = useSelector((state) => state.user)
-  const formattedCreatedAt = new Date(currentUser.createdAt).toLocaleString()
+  const formattedCreatedAt = new Date(currentUser?.createdAt).toLocaleString()
   console.log(currentUser)
   return (
     <div className="mt-16">
@@ -10,10 +10,10 @@ const Home = () => {
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-4xl font-bold text-red-500 text-center mt-5">
             Welcome{" "}
-            <span className="text-blue-500">{currentUser.username}</span>
+            <span className="text-blue-500">{currentUser?.username}</span>
           </h1>
           <img
-            src={currentUser.profilePicture}
+            src={currentUser?.profilePicture}
             alt="profile"
             className="h-28 w-28 rounded-full object-cover my-2 mt-3 shadow-lg"
           />
