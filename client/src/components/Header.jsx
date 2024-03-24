f30fda1eecf0564aef765dc9c537e85828b9862d
g3vind <g3vind@gmail.com>
2024-03-24 14:12:57 +0530

    profile page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,34 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 export default function Header() {
-    return (
-        <div className='bg-slate-200'>
-            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-                <Link to='/'>
-                    <h1 className='font-bold'>Auth App</h1>
-                </Link>
-                <ul className='flex gap-4 '>
-                    <Link to='/'>
-                        <li className='list-none'>Home</li>
-                    </Link>
-                    <Link to='/about'>
-                        <li>About</li>
-                    </Link>
-                    <Link to='/sign-in'>
-                        <li>Sign In</li>
-                    </Link>
-                </ul>
-            </div>
-        </div>
-    );
+  const { currentUser } = useSelector((state) => state.user)
+  return (
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold">Auth App</h1>
+        </Link>
+        <ul className="flex gap-4 ">
+          <Link to="/">
+            <li className="list-none">Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                alt={currentUser.name}
+                className="h-8 w-8 rounded-full object-cover"
+              />
+            ) : (
+              <li>Sign In</li>
+            )}
+          </Link>
+        </ul>
+      </div>
+    </div>
+  )
 }
